3dcabe7aba6baa07c15d8898ef783b5ba82cb255
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-06-26 11:10:04 +0800

    Trie

---
+++ Data_Structure/hdu_1251.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1e6 + 5;
+int tot;
+struct NODE{
+	int ne[26], cnt;
+	void init(){
+		memset(ne, -1, sizeof(ne));
+		cnt = 0;
+	}
+} tr[N];
+void init(){
+	tot = 0;
+}
+void insert(string s){
+	int now = 0;
+	for (int i = 0; i < s.size();i++)
+	{
+		int p = s[i] - 'a';
+		if (~tr[now].ne[p]){
+			now = tr[now].ne[p];
+		}else{
+			tr[now].ne[p] = ++tot;
+			now = tot;
+			tr[now].init();
+		}
+		tr[now].cnt++;
+	}
+}
+int query(string s){
+	int now = 0;
+	for (int i = 0; i < s.size();i++){
+		int p = s[i] - 'a';
+		if (~tr[now].ne[p]){
+			now = tr[now].ne[p];
+		}else
+			return 0;
+	}
+	return tr[now].cnt;
+}
+string s;
+int main(){
+	ios_base::sync_with_stdio(0);
+	tr[0].init();
+	while (getline(cin,s)){
+		if (s.size()==0)
+			break;
+		insert(s);
+	}
+	while (getline(cin,s)){
+		cout << query(s) << endl;
+	}
+	return 0;
+}
