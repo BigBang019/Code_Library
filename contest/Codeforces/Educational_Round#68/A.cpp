cec05ed0f7e01c870f44d8c9896325e1b1508593
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-07-15 19:55:37 +0800

    Create A.cpp

---
+++ contest/Codeforces/Educational_Round#68/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1e5 + 5;
+const int MOD = 1e9 + 7;
+int n, x;
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        cin >> n >> x;
+        cout << x * 2 << endl;
+    }
+    return 0;
+}
