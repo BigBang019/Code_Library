9d87e28a2db74134509d6fbef04d912590665896
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-08-08 21:29:17 +0800

    Create D.cpp

---
+++ contest/2019 Multi-University Training Contest/nowcoder07/D.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+ 
+int main() {
+    int n,p;
+    scanf("%d%d",&n,&p);
+    int chang=0;
+    int c=p;
+    while(p>0){
+        p/=10;
+        chang++;
+    }
+    // printf("%d\n",chang);
+    if(n<chang){
+        printf("T_T\n");
+    }else if(n==chang){
+        printf("%d\n",c);
+    }else{
+        printf("%d",c);
+        for(int i = 0;i<n-chang;i++){
+            printf("0");
+        }
+        printf("\n");
+    }
+    return 0;
+}
