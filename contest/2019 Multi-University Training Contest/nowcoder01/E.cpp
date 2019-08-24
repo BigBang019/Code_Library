7120636a2a6418d158b824645d36864311ed6100
XiaoMingZHM <47984550+XiaoMingZHM@users.noreply.github.com>
2019-08-24 19:42:05 +0800

    DP 思维

---
+++ contest/2019 Multi-University Training Contest/nowcoder01/E.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+using namespace std;
+const int maxn=2005;
+const long long mod=1E9+7;
+
+long long f[maxn][maxn];
+
+int main() {
+	int n,m;
+	while (~scanf("%d%d", &n,&m)) {
+		for (int i=0;i<=n+m;i++) {
+			for (int j=0;j<=n+m;j++) {
+				f[i][j]=0;
+			}
+		}
+		f[0][0]=1;
+		for (int i=0;i<=n+m;i++) {
+			for (int j=0;j<=n+m;j++) {
+				if (i<n || (i-n)<j) {
+					f[i+1][j]+=f[i][j];
+					f[i+1][j]%=mod;
+				}
+				if (j<m || (j-m)<i) {
+					f[i][j+1]+=f[i][j];
+					f[i][j+1]%=mod;
+				}
+			}
+		}
+		printf("%lld\n", f[n+m][n+m]);
+	}
+	return 0;
+}
