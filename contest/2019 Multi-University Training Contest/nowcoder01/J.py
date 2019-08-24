4424409609970d363d56780a2a3197393a0b76e0
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-08-24 19:12:53 +0800

    Create J.py

---
+++ contest/2019 Multi-University Training Contest/nowcoder01/J.py
@@ -0,0 +1,14 @@
+import sys
+while True:
+    line = sys.stdin.readline()
+    if not line:
+        break
+    x, a, y, b = map(int, line.split())
+    l = x*b
+    r = y*a
+    if l == r:
+        print('=')
+    elif l < r:
+        print('<')
+    else:
+        print('>')
