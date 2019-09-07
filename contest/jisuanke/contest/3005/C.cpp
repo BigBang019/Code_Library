467b8f03d73bc25845e9936f0df3f36ef8392d6d
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2019-09-07 18:16:58 +0800

    Sign in

---
+++ contest/jisuanke/contest/3005/C.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main() {
+    int T;scanf("%d",&T);
+    if(T%2==0&& T!=2) printf("YES");
+    else printf("NO");
+    return 0;
+}
